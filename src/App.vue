848c4fd9bf98781321b3832029e7aac70515a55e
Daniel Pinelli <danielpinelli.fullstack@gmail.com>
2021-12-01 10:51:41 -0300

    Showing characters

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,17 @@
-<template></template>
+<template>
+  <div>Rick and Morty</div>
+  <ListCharacters />
+</template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import ListCharacters from "@/components/ListCharacters";
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    ListCharacters,
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
