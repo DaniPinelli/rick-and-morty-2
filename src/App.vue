061a4ccb9e026cd00a1d693051e4089870dc412f
Daniel Pinelli <danielpinelli.fullstack@gmail.com>
2021-12-01 11:26:52 -0300

    Adding styles

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>Rick and Morty</div>
+  <div class="container">
+    <h1>Rick And Morty</h1>
+  </div>
   <ListCharacters />
 </template>
 
@@ -14,4 +16,34 @@ export default {
 </script>
 
 <style lang="scss">
+:root {
+  --background-body: #24282f;
+  --background-card: #3c3e44;
+  --text-white: #ffffff;
+  --text-gray: #c0c0c0;
+  --text-orange: #ff9800;
+}
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  background: var(--background-body);
+  color: var(--text-white);
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+}
+h1 {
+  margin-bottom: 3rem;
+  text-align: center;
+}
+.container {
+  width: 980px;
+  max-width: 90%;
+  margin: 5rem auto;
+}
+img {
+  width: 100%;
+}
 </style>
