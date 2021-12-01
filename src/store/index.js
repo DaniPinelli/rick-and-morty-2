87990b6756fbcc85b28d8dae85dadbc655311627
Daniel Pinelli <danielpinelli.fullstack@gmail.com>
2021-12-01 09:48:33 -0300

    Getting characters

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,34 @@
-import { createStore } from 'vuex'
+import {
+  createStore
+} from 'vuex'
 
 export default createStore({
   state: {
+    characters: [],
+    charactersFilter: []
   },
   mutations: {
+    setCharacters(state, payload) {
+      state.characters = payload
+    },
+    setCharactersFilter(state, payload) {
+      state.charactersFilter = payload
+    }
   },
   actions: {
+    async getCharacters({
+      commit
+    }) {
+      try {
+        const response = await fetch('https://rickandmortyapi.com/api/character/')
+        const data = await response.json()
+        console.log(data)
+        // commit('setCharacters', data.results)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
   },
-  modules: {
-  }
+  modules: {}
 })
